7cbbfabb9282513a64ebcc86eba6683cb9e38fca
phegde <pradnyahegde.null@gmail.com>
2025-03-10 20:32:55 -0400

    Removed atatus error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
 atatus.config('83b01ee93c384802ae90f640f444f3d9').install();
-atatus.notify(new Error('Test Atatus Setup'));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
