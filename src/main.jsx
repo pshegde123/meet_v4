571c335052598d2682425f9390581bc65401164d
phegde <pradnyahegde.null@gmail.com>
2025-03-14 16:54:33 -0400

    Added favicon and service worker files

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
-atatus.config('83b01ee93c384802ae90f640f444f3d9').install();
+import * as serviceWorkerRegistration from '../serviceWorkerRegistration.js';
+
+//atatus.config('83b01ee93c384802ae90f640f444f3d9').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+serviceWorkerRegistration.register();
