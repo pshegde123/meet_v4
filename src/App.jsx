96f439a3f58e0615ad8d429697cce22520b27399
phegde <pradnyahegde.null@gmail.com>
2025-03-17 22:28:12 -0400

    Added EventsGenresChart component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 import { getEvents,extractLocations } from './api';
 import { InfoAlert,ErrorAlert, WarningAlert } from './components/Alert';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import './App.css';
 
 const App = () => {
@@ -46,9 +47,12 @@ const App = () => {
      <div className="alerts-container">
         {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
      </div>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/>
-      <NumberOfEvents numberOfEvents={currentNOE} setNumberOfEvents={setCurrentNOE} setErrorAlert={setErrorAlert}/>
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert}/> 
+      <NumberOfEvents numberOfEvents={currentNOE} setNumberOfEvents={setCurrentNOE} setErrorAlert={setErrorAlert}/>      
+      <div className="charts-container">
+        <CityEventsChart allLocations={allLocations} events={events} />
+        <EventGenresChart events={events} />
+     </div>      
       <EventList events={events}/>      
     </div>
   );
